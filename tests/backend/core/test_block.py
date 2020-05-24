45dc6216b2d1be35bf53753b44ca16140d75c934
dcarpintero <6709785+dcarpintero@users.noreply.github.com>
2020-05-24 12:17:21 +0200

    Add Unit Tests

--- tests/backend/core/test_block.py
+++ tests/backend/core/test_block.py
@@ -5,35 +5,25 @@ from backend.core.block import Block
 from jsonschema import validate
 
 
-def test_block():
-    block = Block(height=1,
-                  data="I'm a mock block",
-                  previous_hash=bck_math.compute_hash('previous block'),
-                  difficulty=1,
-                  nonce=bck_math.compute_nonce())
+block = Block(height=1,
+              data="I'm a mock block",
+              previous_hash=bck_math.compute_hash('previous block'),
+              difficulty=1,
+              nonce=bck_math.compute_nonce())
+
+
+def test_block_instance():
     assert isinstance(block, Block)
 
 
-def test_block_to_json():
+def test_block_schema():
     with open('./schemas/schema_block.json', 'r') as f:
         schema_data = f.read()
         schema_block = json.loads(schema_data)
 
-    block = Block(height=1,
-                  data="I'm a mock block",
-                  previous_hash=bck_math.compute_hash('previous block'),
-                  difficulty=1,
-                  nonce=bck_math.compute_nonce())
-
     block_json = json.loads(block.to_json())
-
     validate(instance=block_json, schema=schema_block)
 
 
 def test_block_repr():
-    block = Block(height=1,
-                  data="I'm a mock block",
-                  previous_hash=bck_math.compute_hash('previous block'),
-                  difficulty=1,
-                  nonce=bck_math.compute_nonce())
     assert block.to_json() == block.__repr__()
--- tests/backend/core/test_node.py
+++ tests/backend/core/test_node.py
@@ -3,23 +3,21 @@ import json
 from backend.core.node import Node
 from jsonschema import validate
 
+node = Node(name='Nakamoto', weight=10)
 
-def test_node():
-    node = Node(name='Nakamoto', weight=10)
+
+def test_node_instance():   
     assert isinstance(node, Node)
 
 
-def test_node_to_json():
+def test_node_schema():
     with open('./schemas/schema_node.json', 'r') as f:
         schema_data = f.read()
         schema_node = json.loads(schema_data)
 
-    node = Node(name='Nakamoto', weight=10)
     node_json = json.loads(node.to_json())
-
     validate(instance=node_json, schema=schema_node)
 
 
 def test_node_repr():
-    node = Node(name='Nakamoto', weight=10)
     assert node.to_json() == node.__repr__()
